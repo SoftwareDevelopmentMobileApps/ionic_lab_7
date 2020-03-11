3a544dd4d562c5f06463e0e4dcad3fef9806a373
Martin Kenirons <martin.kenirons@gmit.ie>
2020-03-11 15:41:23 +0000

    lab upload

--- src/app/leinster-counties/leinster-counties.page.spec.ts
+++ src/app/leinster-counties/leinster-counties.page.spec.ts
@@ -1,19 +1,19 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
-import { HomePage } from './home.page';
+import { LeinsterCountiesPage } from './leinster-counties.page';
 
-describe('HomePage', () => {
-  let component: HomePage;
-  let fixture: ComponentFixture<HomePage>;
+describe('LeinsterCountiesPage', () => {
+  let component: LeinsterCountiesPage;
+  let fixture: ComponentFixture<LeinsterCountiesPage>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomePage ],
+      declarations: [ LeinsterCountiesPage ],
       imports: [IonicModule.forRoot()]
     }).compileComponents();
 
-    fixture = TestBed.createComponent(HomePage);
+    fixture = TestBed.createComponent(LeinsterCountiesPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
